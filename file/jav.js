0b5dfc3e6ebbb2cfa4bb5380f9da9e87939987c0
Y0rFa1se <Y0rFa1se@users.noreply.github.com>
2020-07-10 18:22:59 +0900

    edit

---
+++ file/jav.js
@@ -0,0 +1,17 @@
+function pwcheck(){
+  var inputVal = document.getElementById("pw").value;
+
+  if (inputVal == "wonsoon0523") {
+    window.location = 'https://drive.google.com/file/d/1nO32ZQnpE-69-pkizbVo-buN2jVI0fYA/view';
+  } else {
+    alert("수준ㅋㅋㅋㅋㅋㅋㅋ");
+    alert("그걸틀리네ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
+    alert("어림도없지ㅋㅋㅋㅋㅋ");
+    alert("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
+    alert("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
+  }
+}
+
+function rt(){
+  window.history.back();
+}
